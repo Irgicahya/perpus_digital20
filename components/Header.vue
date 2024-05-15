e204db7d1825714719f32bd431106f415bcaef98
Irgicahya <142205032+Irgicahya@users.noreply.github.com>
2024-05-15 13:13:49 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template> 
+<nav class="navbar bg-primary text-white">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="~/assets/img/LOGO-SMK4_11zon.webp" alt="Bootstrap" width="80">
+  </a> 
+  <div class="title me-auto"> 
+    <h1 class="m-0"> Perpus Digital </h1>
+    <address class="m-0"> 
+      SMKN 4 Tasikmalaya <br> 
+      jl.Depok, Sukanagara, Purbaratu
+    </address>
+  </div>
+  </div>
+</nav>
+</template>
